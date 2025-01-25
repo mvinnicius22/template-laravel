b9cd2bf236b5e9c208abea5ef6ec9132a5273ecc
Marcos Vinicius <mvinnicius22@hotmail.com>
2025-01-25 16:27:37 -0300

    feature: add template files

---
+++ resources/js/Pages/Users/Index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Link :href="route('pages.about')">about</Link>
+        <h1>Users</h1>
+        <div v-for="user in users" :key="user.id">
+            <p>{{ user.name }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import { Link } from "@inertiajs/vue3";
+
+export default {
+    components: {
+        Link,
+    },
+
+    props: {
+        users: Object,
+    },
+};
+</script>
