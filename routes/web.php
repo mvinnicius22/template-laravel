c57dbc1caad2c51f17b8e9c45f4b085d539406b8
Marcos Vinicius <mvinnicius22@hotmail.com>
2025-01-25 16:26:15 -0300

    build: configure inertia

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::inertia('about', 'About')->name('pages.about');
+
+Route::get('/users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', // Permite que o Vite seja acessado de fora do container
+        port: 5173,      // Porta do Vite
+        hmr: {
+            host: 'localhost', // Certifique-se de que aponta para o host local
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        })
     ],
 });
